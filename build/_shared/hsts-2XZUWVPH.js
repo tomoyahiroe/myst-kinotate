fab85a0ae0e111cb0e2bd1d9d1d37722fb61e50d
tomoyahiroe <tomoyahiroe@users.noreply.github.com>
2025-11-02 01:18:46 +0000

    deploy: 1797878ee8486934b18241b30b74f9babab47add

--- build/_shared/hsts-2XZUWVPH.js
+++ build/_shared/hsts-2XZUWVPH.js
@@ -1,1 +1,1 @@
-import{a}from"https://kinotate.com/build/_shared/chunk-65XQQU63.js";import"https://kinotate.com/build/_shared/chunk-2NH4LW52.js";export default a();
+import{a}from"/build/_shared/chunk-65XQQU63.js";import"/build/_shared/chunk-2NH4LW52.js";export default a();
